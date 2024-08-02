1946efdfe5fc06308515fa7bddd7a8cf45771d1e
Patrik Mada (from Dev Box) <patrikmada@microsoft.com>
2024-08-01 18:00:56 -0700

    feat: Improve granularity of exception classes.

--- src/main/scala/exceptions/SLoxException.scala
+++ src/main/scala/exceptions/SLoxException.scala
@@ -4,7 +4,5 @@ trait SLoxException extends Exception {
   protected val exitCode: Int
   protected val exitMessage: String
   protected val methodOnExit: String
-  protected val usage: String
-
-  override def toString: String = s"\nMethod: $methodOnExit\nExit Code: $exitCode -- $exitMessage\n$usage"
+  override def toString: String = s"\nMethod: $methodOnExit\nExit Code: $exitCode -- $exitMessage"
 }
--- src/main/scala/exceptions/UsageException.scala
+++ src/main/scala/exceptions/UsageException.scala
@@ -7,6 +7,9 @@ val Usage: String = "Usage: slox [script]"
 
 case class UsageException(override protected val exitCode: Int = ExUsage,
                           override protected val exitMessage: String,
-                          override protected val usage: String = Usage,
+                          protected val usage: String = Usage,
                           override protected val methodOnExit: String = getCurrentMethodName
-                         ) extends SLoxException
+                         ) extends SLoxException {
+
+  override def toString: String = super.toString + s"\nUsage: $usage"
+}
