ed11db5b4069ff24f565a113944a9cb1fc69a924
Patrik Mada <mada.patrik@gmail.com>
2024-08-03 23:15:27 +0200

    feat: Introduce more robust error handling.

--- src/main/scala/SLox.scala
+++ src/main/scala/SLox.scala
@@ -1,16 +1,30 @@
-import exceptions.SLoxException
+import exceptions.{SLoxException, UsageException}
+import utils.ExitCodes.ExOk
+
+import scala.util.{Success, Failure, Try}
 
 @main def slox(args: String*): Unit = {
-  args.toList match
-    case scriptName :: Nil => runFile(scriptName)
-    case Nil => runPrompt()
-    case exception: SLoxException => exitSLox(exception)
-    case exception => throw Exception(exception.toString)  // TODO: Refactor to not exit so ungracefully.
+  Try {
+    args.toList match {
+      case scriptName :: Nil => runFile(scriptName)
+      case Nil => runPrompt()
+      case _ => throw UsageException()
+    }
+  } match {
+      case Success(_) => exitSLox()
+      case Failure(exception: SLoxException) => exitSLox(exception)
+      case Failure(exception) => println(s"Unhandled exception; failure in Main: $exception")
+  }
 }
 
 def runFile(fileName: String): Unit = ???
 
 def runPrompt(): Unit = ???
+
+def exitSLox(): Unit = {
+  System.out.println("Successful termination.")
+  System.exit(ExOk)
+}
 
 def exitSLox(exception: SLoxException): Unit = {
   System.out.println(exception.toString)
